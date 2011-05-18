1b3fd486e9f9dec0ece51d3cc23bdb5d86825124
Bastian Schumacher <bsch@kupferwerk.net>
2011-05-18 11:31:37 +0200

    start script

---
+++ start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+osascript 2>/dev/null <<EOF
+	tell application "System Events"
+		tell process "Terminal" to keystroke "t" using command down
+	end
+	tell application "Terminal"
+		activate
+		do script with command "cd ~/Projekte/sample_app" in window 1
+	end
+	tell application "Terminal"
+		activate
+		do script with command "rails server" in window 1
+	end
+	
+	tell application "System Events"
+		tell process "Terminal" to keystroke "t" using command down
+	end
+	tell application "Terminal"
+		activate
+		do script with command "cd ~/Projekte/sample_app" in window 1
+	end
+	tell application "Terminal"
+		activate
+		do script with command "bundle exec spork" in window 1
+	end
+	
+	tell application "System Events"
+		tell process "Terminal" to keystroke "t" using command down
+	end
+	tell application "Terminal"
+		activate
+		do script with command "cd ~/Projekte/sample_app" in window 1
+	end
+	tell application "Terminal"
+		activate
+		do script with command "autotest" in window 1
+	end tell
+EOF
